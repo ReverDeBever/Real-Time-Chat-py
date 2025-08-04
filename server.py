51dd7750bad2cad064c28a72c42dfec8f9198f4f
ReverDeBever <revergithub@gmail.com>
2025-08-04 18:27:22 +0200

    switched to asyncio library to handle more clients

--- server.py
+++ server.py
@@ -1,18 +1,4 @@
-import socket
+import asyncio
 
 HOST = "127.0.0.1"  
 PORT = 5000 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reuse of address
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server listening on {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
